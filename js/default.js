91a859df4a13f571c6be64ea95ba7a4402791713
Ege Uz <enkomaun@gmail.com>
2021-05-03 13:39:57 +0300

    js implementation for tabbed article title

--- js/default.js
+++ js/default.js
@@ -26,13 +26,35 @@ let initCategoryDrawers = () =>  {
       drawer.classList.toggle('--isOpen')
     });
   }
+}
 
+
+let formatArticleHeaders = () => {
+  const headers = document.querySelectorAll("h1");
+  headers.forEach(header => {
+    const words = header.innerText.replace(/\n/, " ").split(" ")
+    const medianIndex = txt.length / 2
+    const splitIndex = Math.floor(words.length / 2)
+    const set1 = words.slice(0, splitIndex);
+    const set2 = words.slice(splitIndex);
+    if (set1.join(" ").length < medianIndex) {
+      //move 1 word from set 2 to set 1
+      set1.push(set2.shift())
+    }
+    if (set1.join(" ").length > medianIndex * 1.25) {
+      //move 1 word from set 1 to set 2
+      set2.unshift(set1.pop())
+    }
+    const formattedHTML = `<span>${set1.join(" ")}</span><br><span>${set2.join(" ")}</span>`
+    header.innerHTML = formattedHTML
+  })
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-  console.log('DOM ready');
 
   initCategoryDrawers();
 
-  // smoothScroll();
+  smoothScroll();
+  
+  formatArticleHeaders();
 });
